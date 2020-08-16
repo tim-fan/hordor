853dc8c374e6ef89807635d8ff9051053b67c35c
tim-fan <tim.fanselow@gmail.com>
2020-08-16 01:45:18 +0000

    namespace urls

--- home_inventory/inventory/urls.py
+++ home_inventory/inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name="inventory"
 urlpatterns = [
     path('', views.index, name="index"),
     path('item/<int:item_id>/', views.item, name='item'),
