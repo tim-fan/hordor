c58ffe7d6da9a32a4b27e29fe02a26a38726379b
tim-fan <tim.fanselow@gmail.com>
2020-09-05 03:45:05 +0000

    Added a first-go view of containers as cards

--- home_inventory/inventory/views.py
+++ home_inventory/inventory/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from .models import Item, Container
 from .forms import ItemForm, ContainerForm
 
+
 def index(request):
     item_list = Item.objects.order_by('-creation_date')
     container_list = Container.objects.order_by('-creation_date')
@@ -12,21 +13,36 @@ def index(request):
         'item_list': item_list,
         'container_list': container_list,
     }
-    return render(request, 'inventory/index.html',context)
+    return render(request, 'inventory/index.html', context)
+
 
 class ItemDetailView(generic.DetailView):
     model = Item
+
 
 class ItemListView(generic.ListView):
     def get_queryset(self):
         return Item.objects.order_by('-creation_date')
 
+
 class ContainerDetailView(generic.DetailView):
     model = Container
+
 
 class ContainerListView(generic.ListView):
     def get_queryset(self):
         return Container.objects.order_by('-creation_date')
+
+
+class ContainerTreeView(generic.ListView):
+    template_name = 'inventory/tree_view.html'
+
+    def get_queryset(self):
+        """
+        Queryset is top-level containers - those which are not themselves in a container
+        """
+        return Container.objects.filter(container__isnull=True)
+
 
 class NewItemView(generic.CreateView):
     model = Item
@@ -34,22 +50,29 @@ class NewItemView(generic.CreateView):
     template_name = 'inventory/new_item.html'
     success_url = reverse_lazy('inventory:index')
 
+
 class NewContainerView(generic.CreateView):
     model = Container
     form_class = ContainerForm
     template_name = 'inventory/new_container.html'
     success_url = reverse_lazy('inventory:index')
 
+
 class ItemUpdateView(generic.UpdateView):
     model = Item
     fields = ['name', 'photo', 'container']
     template_name = 'inventory/item_update.html'
+
     def get_success_url(self):
-        return reverse_lazy('inventory:item_detail', kwargs={'pk': self.kwargs['pk']})
+        return reverse_lazy('inventory:item_detail',
+                            kwargs={'pk': self.kwargs['pk']})
+
 
 class ContainerUpdateView(generic.UpdateView):
     model = Container
     fields = ['name', 'photo', 'container']
     template_name = 'inventory/container_update.html'
+
     def get_success_url(self):
-        return reverse_lazy('inventory:container_detail', kwargs={'pk': self.kwargs['pk']})
+        return reverse_lazy('inventory:container_detail',
+                            kwargs={'pk': self.kwargs['pk']})
