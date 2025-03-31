555291d71f09f955401faca6e8e2e84c4af3ad17
tim-fan <tim.fanselow@gmail.com>
2025-03-31 05:54:37 +0100

    Add ItemMovement tracking, for history of item use

--- hordor/inventory/admin.py
+++ hordor/inventory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Item, Container
+from .models import Item, Container, ItemMovement
 admin.site.register(Item)
 admin.site.register(Container)
+admin.site.register(ItemMovement)
--- hordor/inventory/models.py
+++ hordor/inventory/models.py
@@ -25,4 +25,33 @@ class Container(GenericObject):
 
 
 class Item(GenericObject):
-    pass
+
+    def save(self, *args, **kwargs):
+        # if moved container, create ItemMovement record
+        add_movement_record = False
+        if not self.pk:
+            # new item - add a movement into the current container
+            add_movement_record = True
+        else:
+            # existing item - only add movement if container changed
+            original = Item.objects.get(pk=self.pk)
+            add_movement_record = original.container != self.container
+
+        if add_movement_record:
+            ItemMovement.objects.create(
+                item=self,
+                to_container=self.container
+            )
+        super().save(*args, **kwargs)
+
+
+class ItemMovement(models.Model):
+    """
+    For tracking history of item movements between containers
+    """
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    to_container = models.ForeignKey(Container, on_delete=models.SET_NULL, null=True, blank=True, related_name='moved_to')
+    moved_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.item.name} moved to {self.to_container} at {self.moved_at}"
